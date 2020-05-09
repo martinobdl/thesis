f80c2fb7b3a091c251edacaf45c706a7b7c40d3b
akira2357 <martino.n.bernasconi@gmail.com>
2020-05-09 20:44:20 +0200

    109/110...
    fine

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# latex thesis | bibtex thesis | latex thesis | latex thesis | dvips -Ppdf -G0 -ta4 thesis.dvi | ps2pdf14 thesis.ps thesis.pdf
-
-xelatex thesis && bibtex thesis && xelatex thesis && xelatex thesis
